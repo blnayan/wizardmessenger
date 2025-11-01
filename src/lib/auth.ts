8932772322479aed6951ae804375cbe01231b10c
blnayan <52083057+blnayan@users.noreply.github.com>
2025-11-01 16:34:25 -0400

    Updated auth client URL

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,8 +6,6 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "mongodb",
   }),
-  secret: process.env.BETTER_AUTH_SECRET,
-  baseURL: process.env.BASE_URL,
   emailAndPassword: {
     enabled: true,
     minPasswordLength: 8,
